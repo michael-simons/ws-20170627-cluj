f4bfdbb21b0002a0fc2826cc98a3976de852bfbd
Michael J. Simons <michael@simons.ac>
2017-06-16 17:08:17 +0200

    Create first Spring Boot project

---
+++ event-service/src/test/java/ac/simons/ws/cluj/events/EventServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package ac.simons.ws.cluj.events;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
