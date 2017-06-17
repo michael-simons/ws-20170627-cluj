5208e4366ff5ec59215cf2190d1d53eaf373aa06
Michael J. Simons <michael@simons.ac>
2017-06-17 21:20:01 +0200

    Add calendar-service

---
+++ calendar-service/src/main/java/ac/simons/ws/cluj/calendar/CalendarServiceApplication.java
@@ -0,0 +1,12 @@
+package ac.simons.ws.cluj.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarServiceApplication.class, args);
+	}
+}
---
+++ calendar-service/src/test/java/ac/simons/ws/cluj/calendar/CalendarServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package ac.simons.ws.cluj.calendar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalendarServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
