4a28733bfd5309d9225a7c931de86a8923fa6ebd
Michael J. Simons <michael@simons.ac>
2017-06-19 18:07:39 +0200

    Register both calendar and event service with eureka

--- calendar-service/src/main/java/ac/simons/ws/cluj/calendar/CalendarServiceApplication.java
+++ calendar-service/src/main/java/ac/simons/ws/cluj/calendar/CalendarServiceApplication.java
@@ -2,8 +2,10 @@ package ac.simons.ws.cluj.calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CalendarServiceApplication {
 
 	public static void main(String[] args) {
--- event-service/src/main/java/ac/simons/ws/cluj/events/EventServiceApplication.java
+++ event-service/src/main/java/ac/simons/ws/cluj/events/EventServiceApplication.java
@@ -2,8 +2,10 @@ package ac.simons.ws.cluj.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EventServiceApplication {
 
 	public static void main(String[] args) {
