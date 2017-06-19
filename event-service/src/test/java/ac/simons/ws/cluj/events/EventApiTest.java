74e84c538de66afbc5514d8a5ddb0eccc06bdbe5
Michael J. Simons <michael@simons.ac>
2017-06-19 12:12:01 +0200

    Make the test run again by providing a mock bean

--- event-service/src/test/java/ac/simons/ws/cluj/events/EventApiTest.java
+++ event-service/src/test/java/ac/simons/ws/cluj/events/EventApiTest.java
@@ -4,10 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 
 
 @RunWith(SpringRunner.class)
@@ -17,10 +20,14 @@ public class EventApiTest {
     @Autowired
     private MockMvc mvc;
     
+    @MockBean
+    private EventService eventService;
+    
     @Test
     public void getEventsShouldWork() throws Exception {
         this.mvc
                 .perform(MockMvcRequestBuilders.get("/api/events"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name", equalTo("test1")));
     }
 }
