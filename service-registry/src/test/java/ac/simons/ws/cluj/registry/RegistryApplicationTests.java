8694acae143764605876834d40a5ad005f492bf2
Michael J. Simons <michael@simons.ac>
2017-06-19 17:42:39 +0200

    Generate service registry based on Netflix Eureka

---
+++ service-registry/src/test/java/ac/simons/ws/cluj/registry/RegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package ac.simons.ws.cluj.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
