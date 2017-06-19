6506ab09ed34c490e28be1958c4f7e7f7ee0fff5
Michael J. Simons <michael@simons.ac>
2017-06-19 12:34:50 +0200

    Provide an initial dataset

---
+++ event-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO events(held_on, name, created_at, status) VALUES ('2017-06-26 18:30:00', 'Get the most out of your data layer', now(), 'open');
+INSERT INTO events(held_on, name, created_at, status) VALUES ('2017-06-27 09:00:00', 'An introduction to Spring Boot', now(), 'open');
+INSERT INTO events(held_on, name, created_at, status) VALUES ('2017-10-19 18:30:00', 'Die Magie hinter Spring-Boot-Startern', now(), 'open');
+COMMIT;
