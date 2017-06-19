445fcec676a66d2cdea00b59954033602c8a9647
Michael J. Simons <michael@simons.ac>
2017-06-19 18:46:36 +0200

    Provide hystrix dashboard

--- calendar-service/src/main/java/ac/simons/ws/cluj/calendar/CalendarServiceApplication.java
+++ calendar-service/src/main/java/ac/simons/ws/cluj/calendar/CalendarServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class CalendarServiceApplication {
 
 	public static void main(String[] args) {
