0644345bb0fb61c5710b2cb63bc31fbd8a935a6f
Michael J. Simons <michael@simons.ac>
2017-06-19 13:04:15 +0200

    Rewrite service to use repository

--- event-service/src/main/java/ac/simons/ws/cluj/events/EventService.java
+++ event-service/src/main/java/ac/simons/ws/cluj/events/EventService.java
@@ -1,25 +1,17 @@
 package ac.simons.ws.cluj.events;
 
 import java.util.List;
-import javax.persistence.EntityManager;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EventService {
-    private final EntityManager entityManager;
+    private final EventRepository eventRepository;
 
-    public EventService(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
     }
-    
-    @Transactional(readOnly = true)
+
     public List<EventEntity> allEvents() {
-        return this.entityManager
-                .createQuery(
-                        "Select event from EventEntity event order by event.heldOn", 
-                        EventEntity.class
-                )
-                .getResultList();
+        return this.eventRepository.findAllByOrderByHeldOnAsc();
     }
 }
