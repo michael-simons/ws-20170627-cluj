11436f0e50dcdc75bd46fb3efe9c3a2420af1436
Michael J. Simons <michael@simons.ac>
2017-06-19 17:16:51 +0200

    Use JPA/Hibernate only for validating schema
    Move all table creation and inserting to flyway

---
+++ event-service/src/main/resources/db/migration/V0001__Create_table_events.sql
@@ -0,0 +1,8 @@
+create table events (
+    id                 serial primary key,
+    held_on            timestamp not null,
+    name               varchar(512) not null,
+    status             varchar(16) not null default 'open',
+    created_at timestamp not null DEFAULT current_timestamp,
+    CONSTRAINT events_uk UNIQUE (held_on, name)
+);
