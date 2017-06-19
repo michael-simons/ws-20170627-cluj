608ab622eacc643177793efa928333cec4632060
Michael J. Simons <michael@simons.ac>
2017-06-19 11:46:27 +0200

    Fix failing test

--- event-service/src/main/java/ac/simons/ws/cluj/events/EventApi.java
+++ event-service/src/main/java/ac/simons/ws/cluj/events/EventApi.java
@@ -1,7 +1,14 @@
 package ac.simons.ws.cluj.events;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EventApi {
+    @GetMapping("/api/events")
+    public List<EventEntity> getEvents() {
+        return new ArrayList<>();
+    }
 }
