de0a743b0e7f6e2fac3b892f1762691a7f854e45
Michael J. Simons <michael@simons.ac>
2017-06-19 18:46:14 +0200

    Enable circuit breakers

--- calendar-service/src/main/java/ac/simons/ws/cluj/calendar/CalendarServiceApplication.java
+++ calendar-service/src/main/java/ac/simons/ws/cluj/calendar/CalendarServiceApplication.java
@@ -2,10 +2,12 @@ package ac.simons.ws.cluj.calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class CalendarServiceApplication {
 
 	public static void main(String[] args) {
