7eb9c2828ac32a538cfb4fed1ad23271a6c82f3f
Michael J. Simons <michael@simons.ac>
2017-06-19 12:54:37 +0200

    Fix the breaking test

--- event-service/src/main/java/ac/simons/ws/cluj/events/EventService.java
+++ event-service/src/main/java/ac/simons/ws/cluj/events/EventService.java
@@ -1,12 +1,25 @@
 package ac.simons.ws.cluj.events;
 
-import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.EntityManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EventService {
+    private final EntityManager entityManager;
+
+    public EventService(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    
+    @Transactional(readOnly = true)
     public List<EventEntity> allEvents() {
-        return new ArrayList<>();
+        return this.entityManager
+                .createQuery(
+                        "Select event from EventEntity event order by event.heldOn", 
+                        EventEntity.class
+                )
+                .getResultList();
     }
 }
