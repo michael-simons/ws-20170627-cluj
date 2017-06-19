eb3360e575c3c0146493c2c4ddb5fca353864648
Michael J. Simons <michael@simons.ac>
2017-06-19 12:54:29 +0200

    Use a real integration test

--- event-service/src/test/java/ac/simons/ws/cluj/events/EventServiceApplicationTests.java
+++ event-service/src/test/java/ac/simons/ws/cluj/events/EventServiceApplicationTests.java
@@ -1,16 +1,38 @@
 package ac.simons.ws.cluj.events;
 
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class EventServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    public void getEventsShouldWork() {
+        final List<EventEntity> events = this.restTemplate.exchange(
+                "/api/events", 
+                HttpMethod.GET, 
+                null, 
+                new ParameterizedTypeReference<List<EventEntity>>() {}
+        ).getBody();
+        assertThat(
+                events.get(0).getName(), 
+                is(equalTo("Get the most out of your data layer"))
+        );
+    }
 }
