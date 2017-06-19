8f4807096113c69f97b08a545c7504a2063d7566
Michael J. Simons <michael@simons.ac>
2017-06-19 16:55:01 +0200

    Implement create

--- event-service/src/main/java/ac/simons/ws/cluj/events/EventApi.java
+++ event-service/src/main/java/ac/simons/ws/cluj/events/EventApi.java
@@ -1,9 +1,16 @@
 package ac.simons.ws.cluj.events;
 
 import java.util.List;
+import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/api/events")
@@ -17,5 +24,11 @@ public class EventApi {
     @GetMapping
     public List<EventEntity> getEvents() {
         return this.eventService.allEvents();
+    }
+    
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public EventEntity createEvent(final @Valid @RequestBody NewEventCmd newEvent) {
+        return this.eventService.create(newEvent);
     }
 }
