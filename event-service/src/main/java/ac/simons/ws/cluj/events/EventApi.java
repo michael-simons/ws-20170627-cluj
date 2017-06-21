b1399ef9c65bd81509303be3218dc3caea99576a
Michael J. Simons <michael@simons.ac>
2017-06-21 11:38:42 +0200

    Add Apache 2.0 License

--- event-service/src/main/java/ac/simons/ws/cluj/events/EventApi.java
+++ event-service/src/main/java/ac/simons/ws/cluj/events/EventApi.java
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2017 michael-simons.eu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ac.simons.ws.cluj.events;
 
 import java.util.List;
 import javax.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
+/**
+ * @author Michael J. Simons
+ */
 @RestController
 @RequestMapping("/api/events")
 public class EventApi {
